8f6cf7db17ad69deb573dd30de9a2ad97b08ed34
jeretamargo <jeretamargo01@gmail.com>
2026-05-15 11:00:48 -0300

    feat: metodos cvalidate, new y update escritos en view Notificacion

--- app/models.py
+++ app/models.py
@@ -6,17 +6,76 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    notificaciones= 
     pass
 
+"""
+El modelo User en django ya existe y trae muchas caracteristicas ya implementadas, bajo ningun concepto conviene reinventarlo, por lo que se extiende con AbstractUser para agregarle el campo Notificacion
+"""
 class Notificacion(models.Model):
-    usuario= models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario= models.ForeignKey(User, on_delete=models.CASCADE, related_name="notificaciones") #Los "related_name" nos permiten luego poder llamar el campo desde user.notificacion, por ejemplo 
     asunto= models.CharField(max_length=200)
     mensaje = models.TextField()
     leida = models.BooleanField(default=False)
-    fecha_creacion = models.DateTimeField();
+    fecha_creacion = models.DateTimeField(auto_now_add=True) #Guarda automaticamente fecha y hora de creacion
+
+    @classmethod
+    def validate(cls, usuario, asunto, mensaje):
+         errors = []
+
+         if not usuario:
+            errors.append("No existe usario asignado a la notificación.")
+
+         if not asunto or not asunto.strip() :
+            errors.append("El asunto es obligatorio")
+
+         if not mensaje or not mensaje.strip() :
+            errors.append("El mensaje es obligatorio")
+         return errors
+
+    @classmethod
+    def new(cls, usuario, asunto, mensaje):
+        errors = cls.validate(usuario, asunto, mensaje)
+        if errors:
+            return None, errors
+        
+
+        notificacion = cls.objects.create(
+            usuario=usuario,
+            asunto=asunto.strip(),
+            mensaje = mensaje.strip()
+        )
+
+        return notificacion, []
+
+    @classmethod
+    def update(self, asunto, mensaje):
+        errors = self.__class__.validate(asunto, mensaje)
+        if errors:
+            return errors
+        
+        self.asunto = asunto.strip()
+        self.mensaje = mensaje.strip()
+        self.save()
+        return[]
+        
 
 
+class Emprendedor (models.Model):
+    nombre= models.CharField(max_length=200)
+    apellido=models.CharField(max_length=200)
+    email= models.EmailField(max_length=254, unique=True)
+    rubro = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=15)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name="emprendedor")
+
+class Visitante(models.Model):
+    nombre= models.CharField(max_length=200)
+    apellido= models.CharField(max_length=200)
+    email= models.EmailField(max_length=254, unique=True)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE,related_name="visitante")
+    fecha_registro = models.DateField(auto_now_add=True) #Guarda automaticamente fecha de creacion
+
+    
 class Feria(models.Model):
     """Representa una feria con su período, ubicación y capacidad disponible."""
 
