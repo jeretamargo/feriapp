a6bc5b197c194129d1c0e8b9fc883dfb58ab8039
Sebastian Martinez <sebaamartinez54@gmail.com>
2026-05-20 15:41:47 -0300

    Feat: se configura la url de la vista de detalle feria

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("ferias/", views.ListaFeriasView.as_view(), name="lista_ferias"),
     path('emprendedores/lista', user_views.ListaEmprendedoresView.as_view(), name="lista_emprendedores"),
-    path("ferias/nueva/", views.NuevaFeriaView.as_view(), name="nueva_feria")
+    path("ferias/nueva/", views.NuevaFeriaView.as_view(), name="nueva_feria"),
+    path('ferias/<int:pk>/', views.DetalleFeriaView.as_view(), name="detalle_feria"),
     ]
     
     
