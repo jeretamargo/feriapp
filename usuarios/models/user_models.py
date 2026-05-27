379f3d034831c96d148c28dbd3aeeec41cb51d62
jeretamargo <jeretamargo01@gmail.com>
2026-05-27 12:28:57 -0300

    usuario modificado, añadido campo para verificar si el usuario es emprendedor o visitante, para facilitar la creacion del login y la autorizacion

--- usuarios/models/user_models.py
+++ usuarios/models/user_models.py
@@ -5,6 +5,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    TIPO_USUARIO = [
+    ("visitante", "Visitante"),
+    ("emprendedor", "Emprendedor"),
+]
+
+    tipo_usuario = models.CharField(
+        max_length=20,
+        choices=TIPO_USUARIO
+    )
     pass
 
 """
