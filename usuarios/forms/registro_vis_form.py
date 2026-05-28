78867318a474f4afc9b1e794e75a65b49b684087
jeretamargo <jeretamargo01@gmail.com>
2026-05-28 13:35:17 -0300

    feat:registro de visitantes agregado

--- usuarios/forms/registro_vis_form.py
+++ usuarios/forms/registro_vis_form.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from usuarios.models.user_models import User
+from usuarios.models.visitante_models import Visitante
 
 class RegistroVisitanteForm(UserCreationForm):
     class Meta:
@@ -14,35 +15,35 @@ class RegistroVisitanteForm(UserCreationForm):
             "password2",
         )
     
-    username = forms.CharField()
-    widget=forms.TextInput(
+    username = forms.CharField(widget=forms.TextInput(
         attrs={
             "class": "form-control",
             "placeholder": "Usuario"
         }
-    )
-    nombre = forms.CharField()
-    widget=forms.TextInput(
+    ))
+    
+    nombre = forms.CharField(widget=forms.TextInput(
         attrs={
             "class": "form-control",
             "placeholder": "Nombre"
         }
-    )
-    apellido = forms.CharField()
-    widget=forms.TextInput(
+    ))
+    
+    apellido = forms.CharField(widget=forms.TextInput(
         attrs={
             "class": "form-control",
             "placeholder": "Apellido"
         }
-    )
+    ))
+    
 
-    email = forms.EmailField()
-    widget=forms.EmailInput(
+    email = forms.EmailField(widget=forms.EmailInput(
         attrs={
             "class": "form-control",
             "placeholder": "Correo electrónico"
         }
-    )
+    ))
+    
     password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput(
         attrs={
             "class": "form-control",
@@ -55,3 +56,16 @@ class RegistroVisitanteForm(UserCreationForm):
             "placeholder": "Confirmar contraseña"
         }
     ))
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.tipo_usuario = "visitante"
+        if commit:
+         user.save()
+        Visitante.new(
+            usuario=user,
+            nombre=self.cleaned_data["nombre"],
+            apellido=self.cleaned_data["apellido"],
+        
+        )
+        return user
