ba2122ff900e8e196f80d38a2c3f20769c9e4303
jeretamargo <jeretamargo01@gmail.com>
2026-06-04 12:31:42 -0300

    autorizacion y roles aplicados a las vistas, mixins de autorizacion creados para verificar que los usuarios logueados pertenezcan al grupo necesario para acceder a la view, botones de crud ocultos para los usuarios que no son administradores

---
+++ app/mixins/AdminReq.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
+from django.contrib import messages
+
+
+class AdminRequiredMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        messages.error(
+            self.request,
+            "No tenés permisos para acceder a esta sección."
+        )
+        return redirect("/")
