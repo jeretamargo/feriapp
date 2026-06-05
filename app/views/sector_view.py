d60567b32676e03f7976e777779ff22de553f7e6
Sebastian Martinez <sebaamartinez54@gmail.com>
2026-06-05 16:20:22 -0300

    Feat: Se completa las validaciones de sector y feria(form_invalid)

--- app/views/sector_view.py
+++ app/views/sector_view.py
@@ -1,6 +1,9 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 from app.models import Sector
 from app.forms.sector_form import SectorForm
@@ -17,8 +20,16 @@ class SectorListView(LoginRequiredMixin, ListView):
 # Detail View 
 class SectorDetailView(LoginRequiredMixin, DetailView):
     model = Sector
-    template_name = 'sectores/actualizar_sector.html'
+    template_name = 'sectores/detalle_sector.html'
     context_object_name = 'sector'
+    
+    def getcontext_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sector = self.get_object()
+        context['descripcion_completa'] = sector.descripcion_completa()
+        return context
+    
+    
 
 
 # Create View 
@@ -26,9 +37,26 @@ class SectorCreateView(LoginRequiredMixin, CreateView):
     model = Sector
     template_name = 'sectores/nuevo_sector.html'
     #fields = '__all__'
-    success_url = reverse_lazy('sector-list')
+    success_url = reverse_lazy('ferias:lista_sector')
     form_class = SectorForm
+    
+    def form_valid(self, form):
+        """Agrega mensajes de éxito o error al crear un sector."""
+        response = super().form_valid(form)
+        messages.success(self.request, f"El sector '{self.object.nombre}' fue creado correctamente."
+                         f" <a href='{reverse_lazy('ferias:detalle_sector', args=[self.object.pk])}' class='alert-link'>Ver detalle</a>")
+        return response
 
+    def form_invalid(self, form):
+        """Agrega mensajes de error al intentar crear un sector con datos inválidos."""
+        for field, errors in form.errors.items():
+            for error in errors:
+                if field == "__all__":
+                    messages.error(self.request, f"Error general: {error}")
+                else:
+                    messages.error(self.request, f"Error en {field}: {error}")
+        return super().form_invalid(form)
+    
 
 # Update View 
 class SectorUpdateView(LoginRequiredMixin, UpdateView):
@@ -36,11 +64,34 @@ class SectorUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'sectores/actualizar_sector.html'
     #fields = '__all__'
     form_class = SectorForm
-    success_url = reverse_lazy('sector-list')
+    success_url = reverse_lazy('ferias:lista_sector')
+    
+    def form_valid(self, form):
+        """Agrega mensajes de éxito o error al actualizar un sector."""
+        response = super().form_valid(form)
+        messages.success(self.request, f"El sector '{self.object.nombre}' fue actualizado correctamente."
+                         f" <a href='{reverse_lazy('ferias:detalle_sector', args=[self.object.pk])}' class='alert-link'>Ver detalle</a>")
+        return response
+
+    def form_invalid(self, form):
+        """Agrega mensajes de error al intentar actualizar un sector con datos inválidos."""
+        for field, errors in form.errors.items():
+            for error in errors:
+                if field == "__all__":
+                    messages.error(self.request, f"Error general: {error}")
+                else:
+                    messages.error(self.request, f"Error en {field}: {error}")
+        return super().form_invalid(form)
+    
 
 
 # Delete View  
 class SectorDeleteView(LoginRequiredMixin, DeleteView):
     model = Sector
     template_name = 'sectores/borrar_sector.html'
-    success_url = reverse_lazy('sector-list')
+    success_url = reverse_lazy('ferias:lista_sector')
+    
+    def get_success_url(self):
+        messages.warning(self.request, f"El sector '{self.object.nombre}' fue borrado exitosamente.")
+        return super().get_success_url()
+    
